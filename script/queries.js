cf77aee93fc0cdc61620930b259a7681f65bd287
pltommasino <pltommasino@gmail.com>
2024-05-20 15:11:39 +0200

    Add/rm files

--- script/queries.js
+++ script/queries.js
@@ -1,10 +1,17 @@
 // Select the database to use.
 use('BikeStoreDB');
 
-// -----------------------------
-// QUERY 1
-// The 3 most expensive orders
-// -----------------------------
+
+/* 
+-----------------------------------
+#             QUERY 1             #
+#   The 3 most expensive orders   #
+-----------------------------------
+______________________________________________________________________________________________________________________________
+This aggregation pipeline calculates the total cost for each order, sums the costs, and then retrieves the top 3 orders based 
+on their total cost in descending order.
+*/
+
 db.getCollection('Order_items').aggregate([
   //Project: allow us to select the column
   {
@@ -31,4 +38,54 @@ db.getCollection('Order_items').aggregate([
   {
     $limit: 3
   }
+])
+
+
+db.getCollection('Order').aggregate([
+  //JOIN: with Order_items
+  {
+    $lookup: {
+      from: 'Order_items',
+      localField: 'order_id',
+      foreignField: 'order_id',
+      as: 'Order_ID'
+    }
+  },
+  {
+    $unwind: {
+      path: '$Order_ID',
+      preserveNullAndEmptyArrays: false
+    }
+  },
+  //JOIN: with Customers
+  {
+    $lookup: {
+      from: 'Customers',
+      localField: 'customer_id',
+      foreignField: 'customer_id',
+      as: 'Customer_ID'
+    }
+  },
+  {
+    $unwind: {
+      path: '$Customer_ID',
+      preserveNullAndEmptyArrays: false
+    }
+  },
+  //JOIN: with Stores
+  {
+    $lookup: {
+      from: 'Stores',
+      localField: 'store_id',
+      foreignField: 'store_id',
+      as: 'Store_ID'
+    }
+  },
+  {
+    $unwind: {
+      path: '$Store_ID',
+      preserveNullAndEmptyArrays: false
+    }
+  },
+  {}
 ])
